04c6dee1c76af047a3a0dfe195b1839383daaced
shivamsharma8 <113785642+shivamsharma8@users.noreply.github.com>
2025-06-12 09:26:20 +0530

    shivam sharma
    basic concepts and questions

---
+++ day_1-Introduction/ques1.java
@@ -0,0 +1,5 @@
+public class ques1 {
+    public static void main(String[] args) {
+        System.out.println("welcome to bridgelabz");
+    }
+}
---
+++ day_1-Introduction/qus4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class qus4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter radius of the circle: ");
+        double radius = scanner.nextDouble();
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of the circle: " + area);
+    }
+}
